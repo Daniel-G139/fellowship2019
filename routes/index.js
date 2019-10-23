bc146b17735b30595da13b92a12227b137d91b8c
Daniel-G139 <40044858+Daniel-G139@users.noreply.github.com>
2019-10-23 15:34:55 -0700

    Made new route blog.

--- routes/index.js
+++ routes/index.js
@@ -13,6 +13,10 @@ router.get('/cad', function(req, res, next) {
   res.render('cad');
 });
 
+router.get('/blog', function(req, res, next) {
+  res.render('blog');
+});
+
 router.get('/logout', function(req,res,next){
   req.logout();
   req.flash('info', 'You have been logged out.');
